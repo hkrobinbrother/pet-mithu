35d7a5c9a8c379e982a36acc92900a934dbc3c5e
HKrobinvai <hkrobin49@gmail.com>
2025-12-19 20:10:17 +0600

    added the all code

--- mithu-client/src/Provider/AuthProvider.jsx
+++ mithu-client/src/Provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { getAuth } from "firebase/auth";
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import {app} from "../firebase/firebase.config"
 
 export const AuthContext = createContext(null)
@@ -7,6 +7,9 @@ export const AuthContext = createContext(null)
 const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
+
+    const [user,setUser] = useState(null)
+    const [loading,setLoading] = useState(true)
 
 
     const authInfo ={
