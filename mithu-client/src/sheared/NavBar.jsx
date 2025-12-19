627e3ffbd43907f1fb68bb5d4820f20022f09df5
HKrobinvai <hkrobin49@gmail.com>
2025-12-19 18:04:07 +0600

    added the login menu

--- mithu-client/src/sheared/NavBar.jsx
+++ mithu-client/src/sheared/NavBar.jsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router";
 import logoImg from "../../public/images/logopet.png";
+import { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import avatarImg from "../../public/images/avator.jpg"
 
 const NavBar = () => {
+  const [user,setuser] = useState()
+  const [isOpen, setIsOpen] = useState(false);
   const nav = (
     <>
       <li>
@@ -14,20 +19,74 @@ const NavBar = () => {
       <li>
         <Link to="/donation">Donation Campaigns</Link>
       </li>
+      {/* dropdown menu */}
 
-      <li className="ml-14">
-        <details className="dropdown">
-          <summary className="btn m-1">login</summary>
-          <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm ">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>LogOut</a>
-            </li>
-          </ul>
-        </details>
-      </li>
+      <div className="relative">
+        <div className="flex flex-row items-center gap-3">
+          {/* Dropdown btn */}
+          <div
+            onClick={() => setIsOpen(!isOpen)}
+            className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+          >
+            <AiOutlineMenu />
+            <div className="hidden md:block">
+              {/* Avatar */}
+              <img
+                className="rounded-full"
+                referrerPolicy="no-referrer"
+                src={user && user.photoURL ? user.photoURL : avatarImg}
+                alt="profile"
+                height="30"
+                width="30"
+              />
+            </div>
+          </div>
+        </div>
+        {isOpen && (
+          <div className="absolute rounded-xl shadow-md w-[40vw] md:w-[10vw] bg-white overflow-hidden right-0 top-12 text-sm">
+            <div className="flex flex-col cursor-pointer">
+              <Link
+                to="/"
+                className="block md:hidden px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+              >
+                Home
+              </Link>
+
+              {user ? (
+                <>
+                  <Link
+                    to="/dashboard"
+                    className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+                  >
+                    Dashboard
+                  </Link>
+                  <div
+                    onClick={logOut}
+                    className="px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer"
+                  >
+                    Logout
+                  </div>
+                </>
+              ) : (
+                <>
+                  <Link
+                    to="/login"
+                    className="px-4 py-3 hover:bg-neutral-100 transition text-black font-semibold"
+                  >
+                    Login
+                  </Link>
+                  <Link
+                    to="/signup"
+                    className="px-4 py-3 hover:bg-neutral-100 transition text-black font-semibold"
+                  >
+                    Sign Up
+                  </Link>
+                </>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 
