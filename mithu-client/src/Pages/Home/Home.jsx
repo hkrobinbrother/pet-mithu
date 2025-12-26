012e3ad7c42d69b56241fb1aab17b6527af748b1
HKrobinvai <hkrobin49@gmail.com>
2025-12-26 19:15:05 +0600

    slice the pets

--- mithu-client/src/Pages/Home/Home.jsx
+++ mithu-client/src/Pages/Home/Home.jsx
@@ -10,7 +10,11 @@ import WorkingForDog from "./WorkingForDog";
 
 
 
+
 const Home = () => {
+
+
+
     return (
         <div>
             <Helmet>
--- mithu-client/src/Pages/Home/WatingForAdoption.jsx
+++ mithu-client/src/Pages/Home/WatingForAdoption.jsx
@@ -1,34 +1,78 @@
 import { MdOutlinePets } from "react-icons/md";
 import SectionTitle from "../../Components/SectionTitle";
 
+import { useEffect, useState } from "react";
+import { IoSettingsSharp } from "react-icons/io5";
+import { FaCalendarAlt } from "react-icons/fa";
+import { Rating } from "@smastrom/react-rating";
+import { Link, Links } from "react-router";
+
 const WatingForAdoption = () => {
+
+  
+
+
+  const [petss,setPets] = useState([])
+
+  const pets = petss.slice(0,6)
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/petListing")
+    .then(res => res.json())
+    .then(data => setPets(data))
+  },[])
+
   return (
-    <div className="container mx-auto">
-      <SectionTitle
-        firstHeading="Service to Breeds"
-        SecondHeading="Puppies Waiting for Adoption"
-        thirdHeding="The best overall dog DNA test is Embark Breed & Health Kit (view at Chewy), which provides you with a breed brwn and information Most dogs"
-      ></SectionTitle>
-      <div className="flex justify-between mt-10 gap-4">
-        <div className="card bg-base-100 w-96 shadow-sm">
-          <figure>
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Card Title</h2>
-            <p>
-              A card component has a figure, a body part, and inside body there
-              are title and actions parts
-            </p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+     <div className="w-10/12 mx-auto mt-10">
+      <div className="grid md:grid-cols-3 gap-4">
+        {pets.map((pet) => (
+          <div className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+              <img
+                className="w-100 h-100 object-cover"
+                src={pet.photo}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <div>
+                <h1 className="text-2xl font-bold text-red-500 text-center">
+                  {pet.name}
+                </h1>
+              </div>
+              <div className="flex justify-between items-center  font-bold">
+                <h1 className="flex items-center gap-2 text-gray-500 ">
+                  <IoSettingsSharp /> {pet.breed}
+                </h1>
+                <h1 className="flex items-center gap-2 text-gray-500 ">
+                  <FaCalendarAlt />
+                  {pet.birthdate}
+                </h1>
+              </div>
+              <div className="divider"></div>
+              <div className="flex items-center justify-between">
+                <h1 className="">
+                  {" "}
+                  <Rating
+                    style={{ maxWidth: 120 }}
+                    value={pet.rating}
+                    readOnly
+                  />
+                </h1>
+                <h1 className="text-gray-500 font-bold">{pet.location}</h1>
+              </div>
+              <div className="card-actions justify-end">
+                <Link className="text-center flex justify-center items-center   w-full mt-2" to={`/petListing/${pet._id}`}>
+                  <button className="btn bg-amber-500">View Details</button>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
+     <div className=" justify-center items-center flex text-center mt-6">
+       <Link  to={"/petListing"} className="btn bg-amber-400 text-black font-bold justify-center items-center flex text-center">View All</Link>
+     </div>
     </div>
   );
 };
